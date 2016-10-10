73a04c12bc7f2875e926318e03832340139b8f7a
whaph <aykut.yilmaz0@hm.edu>
2016-10-10 21:34:59 +0200

    Aufgabe 3

---
+++ Aufgabe3/recursion.cpp
@@ -0,0 +1,37 @@
+
+//
+// Created by aykut on 10.10.16.
+//
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+#define N_MAX 20
+
+unsigned long long fac(unsigned int n);
+
+int main() {
+    unsigned int n;
+
+    cout << fixed << setprecision(0);
+
+    cout << setw(10) << "n" << setw(30) << "Fakultaet von n" << endl << "\n";
+
+    for (n = 1; n <= N_MAX ; ++n){
+        cout << setw(10) << n << setw(30) << fac(n) << endl;
+    }
+
+    cout << "\nWeiter mit" << "<Return>";
+    std::cin.get();
+
+    return 0;
+}
+
+unsigned long long fac(unsigned int n) {
+    unsigned long long ergebnis;
+    if(n > 0)
+        ergebnis = n * fac(n-1);
+    else
+        ergebnis = 1;
+    return ergebnis;
+}
